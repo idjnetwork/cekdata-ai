08eab7c0e6437c555b1e1088e087a7d91b0cb995
Louis Lugas <louislugas@gmail.com>
2026-03-12 23:39:26 +0700

    add timeout

---
+++ src/routes/chat/+page.server.js
@@ -0,0 +1,14 @@
+// src/routes/+page.server.ts
+import { fail, redirect } from '@sveltejs/kit'
+/** @type {import('./$types').PageServerLoad} */
+
+export const load = async ({ url, locals: { safeGetSession } }) => {
+  const { session } = await safeGetSession()
+
+  // if the user is already logged in return them to the account page
+//   if (session) {
+//     redirect(303, '/chat')
+//   }
+
+  return { url: url.origin }
+}
--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -5,9 +5,6 @@
 
 <script>
     import { onMount } from 'svelte'
-    import {invalidateAll} from '$app/navigation'
-
-    invalidateAll()
 
     // ADD COPY BUTTON ON CHAT REPLIES
     function addCopyButtons() {
@@ -49,35 +46,37 @@
 
     // CREATE CHAT FROM N8N ON ONMOUNT (AFTER ALL DOMs LOADED)
     onMount(()=> {
-        createChat({
-            webhookUrl: 'https://idjn.app.n8n.cloud/webhook/53c136fe-3e77-4709-a143-fe82746dd8b6/chat',
-            mode: 'fullscreen',
-            target: '#chat',
-            defaultLanguage: 'id',
-            initialMessages: [
-                'Halo 👋',
-                'Apa yang bisa saya bantu hari ini?'
-            ],
-            i18n: {
-                id: {
-                    footer: '',
-                    getStarted: 'New Conversation',
-                    inputPlaceholder: 'Tulis pertanyaanmu...',
+        setTimeout(() => {
+            createChat({
+                webhookUrl: 'https://idjn.app.n8n.cloud/webhook/53c136fe-3e77-4709-a143-fe82746dd8b6/chat',
+                mode: 'fullscreen',
+                target: '#chat',
+                defaultLanguage: 'id',
+                initialMessages: [
+                    'Halo 👋',
+                    'Apa yang bisa saya bantu hari ini?'
+                ],
+                i18n: {
+                    id: {
+                        footer: '',
+                        getStarted: 'New Conversation',
+                        inputPlaceholder: 'Tulis pertanyaanmu...',
+                    },
                 },
-            },
-        });
-
-        document.querySelector(".chat-layout").style.height="calc(100vh - 154px - 1rem - 1rem)"
-
-        // observe new messages
-        const observer = new MutationObserver(addCopyButtons);
-
-        // console.log(observer)
-
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
+            });
+    
+            document.querySelector(".chat-layout").style.height="calc(100vh - 154px - 1rem - 1rem)"
+    
+            // observe new messages
+            const observer = new MutationObserver(addCopyButtons);
+    
+            // console.log(observer)
+    
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        }, 500)
     })
 </script>
 
